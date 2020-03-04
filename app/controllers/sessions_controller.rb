754f47b173c3328a0b4d3f6e0be956b53f5e78b5
Ivan <fivanunam@gmail.com>
2020-03-04 14:50:32 -0600

    add layout to signin and the corresponding actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    puts user.name
+  end
+end
