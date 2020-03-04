588ca13e0a72292e8b1451b9d65565cdaac4b753
certilremy <certilremy@gmail.com>
2020-03-04 14:53:07 -0500

    create the user model an validation

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  VALIDATE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :email, presence: true, length: { minimum: 5, maximum: 50 }, format: { with: VALIDATE_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 5, maximum: 20}
+end
