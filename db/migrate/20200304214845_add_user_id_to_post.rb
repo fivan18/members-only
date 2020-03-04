4f8eb7e2e4366e5018e4e14f7232dcdc9bcf36d6
certilremy <certilremy@gmail.com>
2020-03-04 17:20:20 -0500

    add the relation betwwen the post and the user

--- db/migrate/20200304214845_add_user_id_to_post.rb
+++ db/migrate/20200304214845_add_user_id_to_post.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPost < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :user_id, :integer
+    add_column :posts, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_214845) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,7 +26,6 @@ ActiveRecord::Schema.define(version: 2020_03_04_214845) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.integer "user_id"
   end
 
 end
