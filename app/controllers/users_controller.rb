375cceaead4de028dc0cb1974a838eec1d0b9a3e
Ivan <fivanunam@gmail.com>
2020-03-05 10:11:48 -0600

    add create action to create a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+  
+    if @user.save
+      login_user @user
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/index'
   root 'posts#index'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
