45ec67173c960b4f9a6d412ae0823bf92eec8e3a
Ivan <fivanunam@gmail.com>
2020-03-05 10:02:10 -0600

    add signup template

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   get "/logout", to: "sessions#destroy"
+  get '/signup', to: 'users#new'
 
+  resources :users, only: [:new, :index, :create]
   resources :posts, only: [:new, :index, :create, :show]
 
 end
